0df4fe7a514e4b07db43f4f127e166238aef5b2b
raja7393 <30396521+raja7393@users.noreply.github.com>
2017-08-21 22:24:33 +1000

    UsageRecordFactory
    no print statement

--- bcccp/tickets/season/UsageRecordFactory.java
+++ bcccp/tickets/season/UsageRecordFactory.java
@@ -18,7 +18,7 @@ public class UsageRecordFactory implements IUsageRecordFactory {
 
 	@Override
 	public IUsageRecord make(String ticketId, long startDateTime) {
-		System.out.println("dgfjh");
+		
                  hours*rate;
 System.out.println("rate");
 	
