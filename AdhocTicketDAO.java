f0be2ee0daf17d8b84245093c9db3071b3e1ee95
Ammar Zafar <ammar.raja7393@users.noreply.github.com>
2017-08-21 11:16:39 +1000

    Adhoc Ticket Module
    I have made changes in line 13 of the module.

--- AdhocTicketDAO.java
+++ AdhocTicketDAO.java
@@ -10,7 +10,8 @@ public class AdhocTicketDAO  implements IAdhocTicketDAO  {
 	
 	
 	public AdhocTicketDAO(IAdhocTicketFactory factory) {
-		//TODO Implement constructor
+		
+		system.out.println("Ticket number:",+currentTicketNo);
 	}
 
 
