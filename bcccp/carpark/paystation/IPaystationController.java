2b5e2e0148c4a7bb4370093db92c616fd1d22d31
jaggibaidwan66 <30992012+jaggibaidwan66@users.noreply.github.com>
2017-08-21 11:11:09 +1000

    PaystationController
    i have changes in line 20 and 21.

---
+++ bcccp/carpark/paystation/IPaystationController.java
@@ -0,0 +1,8 @@
+package bcccp.carpark.paystation;
+
+public interface IPaystationController {
+	public void ticketInserted(String barcode);
+	public void ticketPaid();
+	public void ticketTaken();
+
+}
---
+++ bcccp/carpark/paystation/PaystationController.java
@@ -0,0 +1,52 @@
+package bcccp.carpark.paystation;
+
+import bcccp.carpark.ICarpark;
+import bcccp.tickets.adhoc.IAdhocTicket;
+
+public class PaystationController 
+		implements IPaystationController {
+	
+	private IPaystationUI ui;	
+	private ICarpark carpark;
+
+	private IAdhocTicket  adhocTicket = null;
+	private float charge;
+	
+	
+
+	public PaystationController(ICarpark carpark, IPaystationUI ui) {
+		//to check the ticket
+
+           System.out.print("Please enter the hours");
+           System.out.print("Please enter the price");
+
+          
+	}
+
+
+
+	@Override
+	public void ticketInserted(String barcode) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void ticketPaid() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void ticketTaken() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
