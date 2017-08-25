23f3d837ed4270e1375eb1d3e993b619eced9437
Sukhjeet Singh Mander <sukhjeet.mander@live.in>
2017-08-25 22:28:16 +1000

    Updated season ticket.java
    Modified line 32 in file

--- bcccp/tickets/season/SeasonTicket.java
+++ bcccp/tickets/season/SeasonTicket.java
@@ -29,7 +29,7 @@ public class SeasonTicket implements ISeasonTicket {
 
 	@Override
 	public String getCarparkId() {
-		// TODO Auto-generated method stub
+		carparkId.this= carparkid;
 		return null;
 	}
 
